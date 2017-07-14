10c75f4a50999276d02bb57876b924abdc0bc0c4
owenliang <120848369@qq.com>
2017-07-14 13:46:49 +0800

    分离logger和sink

---
+++ src/bigpipe/log/async_sink.go
@@ -0,0 +1,75 @@
+package log
+
+import (
+	"time"
+	"fmt"
+	"os"
+)
+
+// 异步日志输出
+type asyncSink struct {
+	curHour int	// 当前的UNIX小时
+	file *os.File	// 文件指针
+	logChan chan[]byte	// 日志队列
+	termChan chan int 	// 退出事件
+}
+
+func (s *asyncSink) Write(p []byte) (n int, err error) {
+	s.logChan <- p
+	return len(p), nil
+}
+
+func (s *asyncSink) rotateFile() {
+	now := time.Now()
+	hour := int(now.Unix() / 3600)
+	if hour != s.curHour {
+		filename := fmt.Sprintf("%s/bigpipe.%02d%02d%02d_%02d.log", gLogger.directory, now.Year(), now.Month(), now.Day(), now.Hour())
+		file, err := os.OpenFile(filename, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0666)
+		if err != nil {
+			return
+		}
+		if s.file != nil {
+			file.Close()
+		}
+		s.curHour = hour
+		s.file = file
+	}
+}
+
+func (s *asyncSink) close(waitChan *chan int) {
+	s.termChan <- 1
+	*waitChan <- 1 // 通知looger退出
+}
+
+func (s *asyncSink) consumeLog() {
+	isClose := false
+
+loop:
+	for true {
+		select {
+		case log := <- s.logChan:
+			s.rotateFile()	// 尝试日志滚动
+			if s.file != nil {
+				s.file.Write(log)
+			}
+		case <- s.termChan:
+			isClose = true
+		default:
+			// 没有遗留数据,并且已关闭
+			if len(s.logChan) == 0 && isClose {
+				if s.file != nil {
+					s.file.Close()
+				}
+				break loop
+			}
+		}
+	}
+}
+
+func newAsyncSink() *asyncSink {
+	sinker := asyncSink{}
+	sinker.termChan = make(chan int, 1)
+	sinker.logChan = make(chan []byte, 100000)
+	go sinker.consumeLog()	// 独立的协程消费管道里的日志
+	return &sinker
+}
--- src/bigpipe/log/logger.go
+++ src/bigpipe/log/logger.go
@@ -1,7 +1,6 @@
 package log
 
 import (
-	"os"
 	"log"
 	"fmt"
 	"time"
@@ -12,23 +11,15 @@ import (
 // 日志管理
 type logger struct {
 	log *log.Logger	// 内部挂载了asyncSink
-	sinker iSink	// 日志输出类（接口抽象）
+	sinker ISink	// 日志输出类（接口抽象）
 	level int // 日志级别
 	directory string  // 存储目录
 }
 
 // sink接口
-type iSink interface {
+type ISink interface {
 	io.Writer	// 继承
 	close(*chan int)	// 关闭
-}
-
-// 异步日志输出
-type asyncSink struct {
-	curHour int	// 当前的UNIX小时
-	file *os.File	// 文件指针
-	logChan chan[]byte	// 日志队列
-	termChan chan int 	// 退出事件
 }
 
 const (
@@ -78,66 +69,6 @@ func (logger *logger)queueLog(level int, userLog *string) {
 	now := time.Now()
 	logger.log.Printf("[%s][%04d-%02d-%02d %02d:%02d:%02d:%03d] %s", levelStr[level],
 		now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), now.Second(), now.Nanosecond() / 1000000, *userLog)
-}
-
-func (s *asyncSink) Write(p []byte) (n int, err error) {
-	s.logChan <- p
-	return len(p), nil
-}
-
-func (s *asyncSink) rotateFile() {
-	now := time.Now()
-	hour := int(now.Unix() / 3600)
-	if hour != s.curHour {
-		filename := fmt.Sprintf("%s/bigpipe.%02d%02d%02d_%02d.log", gLogger.directory, now.Year(), now.Month(), now.Day(), now.Hour())
-		file, err := os.OpenFile(filename, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0666)
-		if err != nil {
-			return
-		}
-		if s.file != nil {
-			file.Close()
-		}
-		s.curHour = hour
-		s.file = file
-	}
-}
-
-func (s *asyncSink) close(waitChan *chan int) {
-	s.termChan <- 1
-	*waitChan <- 1 // 通知looger退出
-}
-
-func (s *asyncSink) consumeLog() {
-	isClose := false
-
-	loop:
-	for true {
-		select {
-			case log := <- s.logChan:
-				s.rotateFile()	// 尝试日志滚动
-				if s.file != nil {
-					s.file.Write(log)
-				}
-			case <- s.termChan:
-				isClose = true
-			default:
-				// 没有遗留数据,并且已关闭
-				if len(s.logChan) == 0 && isClose {
-					if s.file != nil {
-						s.file.Close()
-					}
-					break loop
-				}
-		}
-	}
-}
-
-func newAsyncSink() *asyncSink {
-	sinker := asyncSink{}
-	sinker.termChan = make(chan int, 1)
-	sinker.logChan = make(chan []byte, 100000)
-	go sinker.consumeLog()	// 独立的协程消费管道里的日志
-	return &sinker
 }
 
 func FATAL(format string, v ...interface{}) {
